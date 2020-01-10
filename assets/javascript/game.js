9d39a34bb69321d25b1fa75ca188f02c5f7fae56
Joel Scott <jscottusf@gmail.com>
2020-01-09 20:23:54 -0500

    js stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wordChoices = ["fish", "shark", "whale", "limefish", "orca", "porpoise"]
+var wordBoard;
+var wordBoardText;
+var guesses = [];
+var guessesText;
+
+//assign variables to text
+wordBoardText = document.getElementById("wordBoardText");
+guessesText = document.getElementById("guessesText");
+
+document.onkeyup = function(event) {
+    
+}
